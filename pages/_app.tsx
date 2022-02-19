37b25556c99c93f8cac9bc941a5c283b8e15ce40
Hari Surrisyad <hari.surrisyad@gmail.com>
2022-02-19 20:24:41 +0700

    install and settingup apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { ApolloProvider } from '@apollo/client';
+import client from 'services/apollo-client';
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <ApolloProvider client={client}>
+    <Component {...pageProps} />
+  </ApolloProvider>
+);
 
 export default MyApp;
